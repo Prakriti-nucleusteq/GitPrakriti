3a37a94c982c9c35ea94dd4ca22cfa661b0d6fef
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2022-11-18 15:56:52 +0530

    Update ticket-service.js

--- demo/src/services/ticket-service.js
+++ demo/src/services/ticket-service.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 export const createTicket=(ticketData)=>{
-    return axios.post('http://localhost:9090/api/ticket',ticketData).then(response=>response.data)
+    return axios.post('http://54.95.126.93:9090/api/ticket',ticketData).then(response=>response.data)
 }
 
