0b0a81a4cdef6f9a41ac0df7750d7bd15c60f734
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2022-11-18 15:56:35 +0530

    Update subCategory-service.js

--- demo/src/services/subCategory-service.js
+++ demo/src/services/subCategory-service.js
@@ -2,14 +2,14 @@ import axios from "axios";
 // import Axios from "axios";
 
 export const loadAllSubCategories=()=>{
-    return axios.get('http://localhost:9090/api/categories/subcategories').then(response=>{return response.data})
+    return axios.get('http://54.95.126.93:9090/api/categories/subcategories').then(response=>{return response.data})
 }
 
 export const loadAllSubCategoriesByCategory=(categoryId)=>{
-    return axios.get(`http://localhost:9090/api/categories/subcategories/${categoryId}`).then(response=>{return response.data})
+    return axios.get(`http://54.95.126.93:9090/api/categories/subcategories/${categoryId}`).then(response=>{return response.data})
 }
 
 
 export const loadCategoryId=(categoryTitle)=>{
-    return axios.get(`http://localhost:9090/api/categories/${categoryTitle}`).then(response=>{return response.data})
+    return axios.get(`http://54.95.126.93:9090/api/categories/${categoryTitle}`).then(response=>{return response.data})
 }
