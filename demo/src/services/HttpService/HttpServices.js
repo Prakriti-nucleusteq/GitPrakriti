f73ab09c6e01ed542a89b10df4fca1084e582bb5
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2022-11-18 15:57:09 +0530

    Update HttpServices.js

--- demo/src/services/HttpService/HttpServices.js
+++ demo/src/services/HttpService/HttpServices.js
@@ -1,6 +1,6 @@
 const axios = required ('axios')
 
 const updateTicket = (body) => {
-    return axios.put('http://localhost:8080/ticket/update', body)
+    return axios.put('http://54.95.126.93:8080/ticket/update', body)
 }
 export default updateTicket;
