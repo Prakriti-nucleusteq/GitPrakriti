4bc48f78273fe98b053c71947a6ffc125d8d934b
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2022-11-21 13:09:42 +0530

    Update category-service.js

--- demo/src/services/category-service.js
+++ demo/src/services/category-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //import Axios from "axios";
-import { BASE_URL } from "../Api/EndPoint/index.js";
+import { BASE_URL } from "../Api/EndPoint/Index.js";
 
 export const loadAllCategories=()=>{
     return axios.get(BASE_URL+'/api/categories/').then(response=>{return response.data})
